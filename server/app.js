180449f25d201370799e4014e08a18cf2b9b2b4e
AlAswaad99 <dannyboy9917@gmail.com>
2022-12-20 04:11:38 +0300

    deployment on netlify fix2

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const serverless = require('serverless-http');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.static(path.join(__dirname, '../build')));
@@ -19,4 +21,8 @@ const libraryRouter = require("./libraryController");
 app.use("/auth", authRouter);
 app.use("/add", libraryRouter);
 
+app.use('/.netlify/functions/app', authRouter);
+app.use('/.netlify/functions/app', libraryRouter);
+
 module.exports = app;
+module.exports.handler = serverless(app);
